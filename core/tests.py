3b8025ed6e4803dc1a6ffd805703576aa26911c0
Kenneth Reitz <me@kennethreitz.org>
2017-04-28 12:06:19 -0400

    Revert "try"
    This reverts commit da8a2d80ac2cfb40db3760a32bac191b16e75345.

--- core/tests.py
+++ core/tests.py
@@ -1,26 +1,5 @@
 from django.test import TestCase
 
-from ghost.ext.django.test import GhostTestCase
-
-
-class SessionSecurityTestCase(GhostTestCase):
-    # Enable display (runs headless with False)
-    display = True
-
-    # Default port for LiveServerTestCase, from django docs:
-    # https://docs.djangoproject.com/en/1.4/topics/testing/#django.test.LiveServerTestCase
-    port = 8081
-
-    # Shortcut for open
-    def open(self, url):
-        self.ghost.open('http://localhost:%s%s' % (self.port, url))
-
-    def test_ghost(self):
-        self.open('/admin/')
-        self.ghost.wait_for_selector('input[name=username]')
-        self.assertTrue(True)
-
-
 class TestSanity(TestCase):
     """docstring for TestSanity"""
 
